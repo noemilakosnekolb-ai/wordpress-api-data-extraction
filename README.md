caed87543bfadaa1598932f8abc88530b70cf6f2
noemilakosnekolb-ai <noemi.lakosnekolb@gmail.com>
2026-01-05 04:30:02 +0100

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# wordpress-api-data-extraction
-Extracting structured category data from a WordPress REST API response and converting it into a clean, business-friendly Excel format.
---
+++ src/API_export_to_excel.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import requests
+
+#Asking the categories
+url = "http://www.gmkitchen.hu/wp-json/wp/v2/categories?per_page=100"
+response = requests.get(url)
+
+
+if response.status_code ==200:
+    categories = response.json()
+    #Making list from the datas we need
+    data = []
+    for cat in categories:
+        data.append({
+            "Név": cat['name'],
+            "ID": cat['id']
+        })
+        print(data)
+    #put it in a dataframe
+    df = pd.DataFrame(data)
+    #export to Excel
+    df.to_excel("output\categories.xlsx", index=False) 
+    print("Succesfully export: categories.xlsx")  
+else:
+    print("Error:", response.status_code)
